2d3fd55c2c8e177c12a6247b43a4c3718ff899f8
Kingyaga <marcellusobiajulu@gmail.com>
2024-03-05 18:20:51 +0100

    Please work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,15 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """ Get number of subscribers for a subreddit """
     try:
-        headers = {'User-Agent': '0x16-api_advanced:project:\
-        v1.0.0 (by /u/Moh\'King Yaga'}
-        count = requests.get('https://www.reddit.com/r/{}/about.json'.format(
-                             subreddit), headers=headers)
-        count.raise_for_status()
-        return count.json().get('data', {}).get('subscribers', 0)
-    except Exception:
+        headers = {
+            'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/Moh\'King Yaga'
+        }
+        url = f'https://www.reddit.com/r/{subreddit}/about.json'
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json().get('data', {}).get('subscribers', 0)
+    except requests.RequestException as e:
         return 0
