202700788d3f7c50faf0102531d27b003b31d463
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2024-03-05 16:32:04 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+""" Advance API module """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """ Get number of subscribers for a subreddit """
+    try:
+        headers = {'User-Agent': '0x16-api_advanced:project:\
+        v1.0.0 (by /u/Moh\'King Yaga'}
+        count = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+                             subreddit), headers=headers)
+        count.raise_for_status()
+        return count.json().get('data', {}).get('subscribers', 0)
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
