43043b979523dcec87d21c0982469e0b254bfe01
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2024-03-05 16:20:10 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Function to query subscribers on a given Reddit subreddit.
+"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """
+    Return the total number of subscribers on a given subreddit.
+    If not a valid subreddit, return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    try:
+        data = response.json()
+        return data["data"]["subscribers"]
+    except (ValueError, KeyError):
+        return 0
