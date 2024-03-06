da90f0c170f53eca8ee2242eaf980c3e3cdad105
Kingyaga <marcellusobiajulu@gmail.com>
2024-03-06 20:23:43 +0100

    Please work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-""" Advance API module """
-import json
+"""Advance API module"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """ Get number of subscribers for a subreddit """
+    """Get the number of subscribers for a subreddit"""
     try:
-        headers = {'User-Agent': '0x16-api_advanced:project:\
-        v1.0.0 (by /u/Moh\'King Yaga'}
-        count = requests.get('https://www.reddit.com/r/{}/about.json'.format(
-                             subreddit), headers=headers)
+        headers = {'User-Agent': '0x16-api_advanced:project: v1.0.0 (by /u/Moh\'King Yaga'}
+        with requests.Session() as session:
+            count = session.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers)
         count.raise_for_status()
         return count.json().get('data', {}).get('subscribers', 0)
-    except Exception:
+    except (requests.exceptions.RequestException, ValueError, KeyError):
         return 0
