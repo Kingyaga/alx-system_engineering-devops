e27ae5bc70ac6f9b29ead2b438bcdc63aa11ad91
Kingyaga <marcellusobiajulu@gmail.com>
2024-03-06 20:21:15 +0100

    Please work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
+""" Advance API module """
+import json
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-
-
 def number_of_subscribers(subreddit):
-    '''Retrieves the number of subscribers in a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
-    return
+    """ Get number of subscribers for a subreddit """
+    try:
+        headers = {'User-Agent': '0x16-api_advanced:project:\
+        v1.0.0 (by /u/Moh\'King Yaga'}
+        count = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+                             subreddit), headers=headers)
+        count.raise_for_status()
+        return count.json().get('data', {}).get('subscribers', 0)
+    except Exception:
+        return 0
