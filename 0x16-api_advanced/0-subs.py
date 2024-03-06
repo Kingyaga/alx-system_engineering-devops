42004963ccebba00c07ef14720be69ff2ba8633d
Kingyaga <marcellusobiajulu@gmail.com>
2024-03-06 20:49:39 +0100

    Please work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,18 +9,10 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     If the subreddit is invalid, returns 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "0x16-api_advanced:project: v1.0.0 (by /u/Moh'King Yaga)"}
-
     try:
-        with requests.Session() as session:
-            response = session.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException:
-        return 0
-
-    try:
-        data = response.json()
-        return data["data"]["subscribers"]
-    except (ValueError, KeyError):
+        headers = {'User-Agent': '0x16-api_advanced:project: v1.0.0 (by /u/Moh\'King Yaga'}
+        count = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
+        count.raise_for_status()
+        return count.json().get('data', {}).get('subscribers', 0)
+    except (requests.exceptions.RequestException, ValueError, KeyError):
         return 0
