a78e66095508bd96dcdac49307fc4cb009285592
Kingyaga <marcellusobiajulu@gmail.com>
2024-03-06 20:56:24 +0100

    Please work

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-"""Module for querying the Reddit API for a given subreddit's subscriber count."""
+"""Advance API module"""
+import json
 import requests
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit. If the subreddit is invalid, returns 0."""
+    """Get number of subscribers for a subreddit"""
     try:
         headers = {'User-Agent': '0x16-api_advanced:project: v1.0.0 (by /u/Moh\'King Yaga'}
         count = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
